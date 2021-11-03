44ab792234f5b2af36ac681975a22a26619a100c
Craig Vidler <craigvidler@Craigs-MacBook-Pro-2.local>
2021-11-03 16:37:07 +0000

    Added import for accordion data into ts file, removed oninit - unused

--- technical-test/src/app/accordion/accordion.component.ts
+++ technical-test/src/app/accordion/accordion.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { AccordionData } from './../models/accordion-data.model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
   styleUrls: ['./accordion.component.scss']
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent {
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
